26f3737a0d7c95f04f295008262d1b1460ff3964
Mokerstier <pannekoek-wouter@hotmail.com>
2019-10-15 23:51:24 +0200

    request to adafruit is working

--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,41 @@
 const fetch = require("node-fetch");
 
-async function getData(req, res){
+async function getData(req, res) {
     let dataCall = await fetch(
         `https://io.adafruit.com/api/v1/feeds/streemdata/data/last?x-aio-key=${process.env.ADA_FEED_KEY}`
-        );
-    let data = await dataCall.json();
-    console.log(data);
-    res.render('mapBox.ejs',{
+    );
+    let lonCall = await fetch(
+        `https://io.adafruit.com/api/v1/feeds/londata/data/last?x-aio-key=${process.env.ADA_FEED_KEY}`
+    );
+    let latCall = await fetch(
+        `https://io.adafruit.com/api/v1/feeds/latdata/data/last?x-aio-key=${process.env.ADA_FEED_KEY}`
+    );
+    let valueData = await dataCall.json();
+    let lonData = await lonCall.json();
+    let latData = await latCall.json();
+    console.log(lonData);
+    console.log(latData);
+    res.render('mapBox.ejs', {
         title: "Mapping around",
-        data: data
+        valueData: valueData.value,
+        lonData: lonData.value,
+        latData: latData.value
+
     })
-    return data;  
-    
+    return valueData;
+
 }
 
 function routes() {
-   
+
     const exRoutes = require('express').Router();
     const bodyParser = require('body-parser');
     const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
     require('dotenv').config();
 
-    
-    
+
+
     exRoutes.get(`/`, getData);
 
     return exRoutes;
